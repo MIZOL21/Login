3c244350167e5198cc75b62c949729c26edc5262
MIZOL21 <careguamaicol1@gmail.com>
2026-04-18 15:01:00 -0500

    Login_mejorado

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -20,7 +20,23 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  usuario: string = '';
+  password: string = '';
   onLogin() {
+    if (this.usuario === '' || this.password === '') {
+
+      alert('Debes ingresar usuario y contraseña');
+
+      return; // Detiene el proceso
+
+    }
     alert('Se procederá a validar las credenciales de acceso');
+    console.log('Usuario:', this.usuario);
+    console.log('Password:', this.password);
+  }
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
   }
 }
